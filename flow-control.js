eca8a94c64fb8d95d81ed4028cbcfd8c51bef984
Emma Nam <en2458@gmail.com>
2020-11-14 01:27:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,7 +4,7 @@ function basicTeenager(age) {
   }
 
 function teenager(age) {
-  if (age >= 13 && age <= 19){
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else {
     return "You are not a teenager";
@@ -12,5 +12,11 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  } else if (age <= 12) {
+    return "You are a kid"
+  } else {
+    "You are a grownup"
+  }
 }
